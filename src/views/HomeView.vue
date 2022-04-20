41574d64fefa5987b7639278047f396d00229b59
liuminqing <liuminqing@hikvision.com.cn>
2022-04-20 21:02:53 +0800

    update

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,5 @@
 <template>
   <div class="home">
-    <div>{{ test }}</div>
     <img alt="Vue logo" width="100" src="@/assets/demo.svg" />
     <HelloWorld :msg="msg" />
   </div>
@@ -18,9 +17,5 @@ export default defineComponent({
 });
 </script>
 <script setup lang="ts">
-const test = ref(0);
-setInterval(() => {
-  test.value++;
-}, 1000);
 const msg = ref<string>("Welcome to Your Vue.js + TypeScript App");
 </script>
